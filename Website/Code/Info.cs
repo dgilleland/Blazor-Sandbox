007975d7654b22634f352180b48af78324385946
Dan Gilleland <dgilleland@nait.ca>
2024-03-09 08:06:08 -0700

    Explorations, Experimentations, and some Markdig

---
+++ Website/Code/Info.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp;
+
+public class ClassInfo
+{
+    private List<string> _ClassNames = new();
+    public IEnumerable<string> ClassNames => _ClassNames;
+    public ClassInfo(Type classType)
+    {
+        Type? type = classType;
+        while (type is not null)
+        {
+            _ClassNames.Add(type.Name);
+            type = type.BaseType;
+        }
+    }
+}
